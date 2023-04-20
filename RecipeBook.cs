350efdbc7cbd7c7a05df9c22e4cb9f817fc6f545
N-Nefale <104350552+N-Nefale@users.noreply.github.com>
2023-04-20 07:19:36 +0000

    update
    Added outline of how the program will function

--- RecipeBook.cs
+++ RecipeBook.cs
@@ -3,13 +3,42 @@
 
 
     public static void Main()
-    { 
-    
-    
-    
-    
-    
-    
+    {
+
+        /*Get the user to make a selection from the options provided below
+            * Menu:
+            *      1.New recipe
+            *      2.View recipe
+            *      3.Exit
+            * 
+            */
+
+        /*Collect any information related to the recipe
+        * New recipe:
+        *          1.recipe name
+        *          2.number of ingredients
+        *          3.ingredients name
+        *          4.quantity
+        *          5.unit of measurement
+        *          6.number of  steps
+        *          7.input steps
+        * 
+        */
+
+
+        /*Dispplay the recipe in full 
+         * View recipe:
+         *      scale- 
+         *          1. x1
+         *          2. x2
+         *          3. x3
+         *          4. x4
+         * 
+         */
+
+
+
+
     }
 
 
