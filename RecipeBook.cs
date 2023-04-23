48b333405a3b23ccad658cbbeed4fce498b8df57
N-Nefale <104350552+N-Nefale@users.noreply.github.com>
2023-04-23 17:07:00 +0000

    update new recipe()
    Added comments

--- RecipeBook.cs
+++ RecipeBook.cs
@@ -16,73 +16,65 @@ class Recipe
 class RecipeBook
 {
 
-
+    // Define a public static method called "Main"
     public static void Main()
     {
 
+
+
+
         /*Get the user to make a selection from the options provided below
-            * Menu:
-            *      1.New recipe
-            *      2.View recipe
-            *      3.Exit
-            * 
-            */
-
-        string input = null;
-        do
-        {
-
-            Console.WriteLine("Menu:\r\n         1* New recipe\r\n         2* View recipe\r\n         3* Clear Data\\r\\n         3* Exit");
-            input = Console.ReadLine();
-
-            if (input.Equals("1"))
-            {
-
-                newRecipe();
-
-            }
-            else
-            if (input.Equals("2"))
-            {
-
-                //viewRecipe();
-
-            }
-            else
-            if (input.Equals("3"))
-            {
-
-                //clearData();
-
-            }
-            else
-            {
-
-                Console.WriteLine("\nINCORRECT SELECTION!!!!!\n");
-            
-            }
-
-        } while (input != "4");
-
-
-        /*Dispplay the recipe in full 
-         * View recipe:
-         *      scale- 
-         *          1. x1
-         *          2. x2
-         *          3. x3
-         *          4. x4
+         * Menu:
+         *      1.New recipe
+         *      2.View recipe
+         *      3.Exit
          * 
          */
 
 
+        // Initialize a string variable called "input" with a null value
+        string input = null;
 
+        // Start a loop that continues until the user enters "4"
+        while (input != "4")
+        {
+            // Display a menu of options to the user
+            Console.WriteLine("select an option:");
+            Console.WriteLine("1. Input new recipe");
+            Console.WriteLine("2. View recipe");
+            Console.WriteLine("3. Exit");
 
+            // Read the user's input from the console
+            input = Console.ReadLine();
+
+            // Use a switch statement to determine which action to take based on the user's input
+            switch (input)
+            {
+                case "1":
+                    // If the user entered "1", call a method called "newRecipe()"
+                    newRecipe();
+                    break;
+                case "2":
+                    // If the user entered "2", call a method called "viewRecipe()"
+                    viewRecipe();
+                    break;
+                case "3":
+                    // If the user entered "3", exit the program
+                    Environment.Exit(-1);
+                    break;
+                default:
+                    Console.WriteLine("Incorrect selection");
+                    break;
+
+            }
+        }
     }
 
 
+    // Define a public static method called "newRecipe"
     public static void newRecipe()
     {
+
 
         /*Collect any information related to the recipe
          * New recipe:
@@ -96,50 +88,79 @@ class RecipeBook
          * 
          */
 
-        Console.WriteLine("New recipe:\r\n Please enter the name of the recipe:");
-        string rName = Console.ReadLine();
-        Console.WriteLine("");
 
+        // Ask the user to enter the name of the recipe
+        Console.WriteLine("Please enter the name of the recipe: ");
+        string recipeName = Console.ReadLine();
 
-        Console.WriteLine("\r         How many ingredients are needed for this recipe:");
+        // Ask the user to enter the number of ingredients in the recipe
+        Console.WriteLine("Please enter the number of ingredients in the recipe: ");
         int numOfIngredients = int.Parse(Console.ReadLine());
+        int a = 0;
 
-        String[] ingrNames = new string[numOfIngredients];
-        double[] ingrQuantity = new double[numOfIngredients];
-        String[] ingrUntsOfMeasuremnt = new string[numOfIngredients];
+        // Create arrays to hold the ingredients, quantities, and units of measurement
+        string[] ingredientsList = new string[numOfIngredients];
+        string[] ingredientsQuantity = new String[numOfIngredients];
+        string[] ingredientsMeasurement = new string[numOfIngredients];
 
-        for (int a = 0; a < ingrNames.Length; a++)
+        // Use a while loop to prompt the user to enter information for each ingredient
+        while (a < numOfIngredients)
         {
+            // Ask the user to enter the ingredient name
+            Console.WriteLine("Please enter the ingredient name: ");
+            ingredientsList[a] = Console.ReadLine();
 
-            Console.WriteLine("Please enter ingredient name:");
-            ingrNames[a] = Console.ReadLine();
+            // Ask the user to enter the quantity of the ingredient
+            Console.WriteLine("Please enter the quantity of the ingredient: ");
+            ingredientsQuantity[a] = Console.ReadLine();
 
-            Console.WriteLine("Please enter how much ingredient of this ingerdient is needed:");
-            ingrQuantity[a] = double.Parse(Console.ReadLine());
+            // Ask the user to select the unit of measurement for the ingredient
+            Boolean flag1 = false;
+
+            do
+            {
+                Console.WriteLine(
+                    "Please enter the unit of measurementConsole.WriteLine:\r\n 1. teaspoon\r\n 2. tablespoon\r\n 3. cup\r\n 4. quart");
+                string choice = Console.ReadLine();
+
+                // Use a switch statement to set the unit of measurement based on the user's choice
+                if (choice.Equals("1"))
+                {
+
+                    ingredientsMeasurement[a] = "teaspoon";
+                    flag1 = true;
+
+                }
+                else if (choice.Equals("2"))
+                {
+
+                    ingredientsMeasurement[a] = "tablespoon";
+                    flag1 = true;
+
+                }
+                else if (choice.Equals("3"))
+                {
+
+                    ingredientsMeasurement[a] = "cup";
+                    flag1 = true;
+
+                }
+                else if (choice.Equals("4"))
+                {
+
+                    ingredientsMeasurement[a] = "quart";
+                    flag1 = true;
+
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect selection");
+                }
 
 
-            Console.WriteLine("Please select a unit of measurement for this ingredient:\r\n 1.Milligrams(mg)\r\n 2.Grams(g)\r\n 3.");
-            //ingrUntsOfMeasuremnt[i]
+            } while (flag1 != true);
 
-        }
-
-
-
-        Console.WriteLine("Please enter the number of step in this recipe");
-        int numOfSteps = int.Parse(Console.ReadLine());
-        string[] steps = new string[numOfSteps];
-        
-        int stepProg = 1;
-
-        for (int b = 0; b < steps.Length; b++)
-        {
-
-
-            Console.WriteLine("Please enter step " + stepProg + " of the recipe: ");
-            steps[b] = Console.ReadLine();
-
-            b++;
-            stepProg++;
+            a++;
 
         }
 
@@ -151,7 +172,21 @@ class RecipeBook
 
 
 
+    public static void viewRecipe()
+    {
 
+        /*Dispplay the recipe in full 
+             * View recipe:
+             *      scale- 
+             *          1. x1
+             *          2. x2
+             *          3. x3
+             *          4. x4
+             * 
+             */
+
+
+    }
 
 
 }
